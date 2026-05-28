a63894df0d791c498e2413b4bfddd2dee58bd3c8
kimegami_shinji <workshinji2023@gmail.com>
2026-05-28 17:48:06 +0900

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    
     <meta charset="utf-8">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,6 +9,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
