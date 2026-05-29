d43c3ed43951ae5564cb0b15c61f1953e7a3dd5d
kimegami_shinji <workshinji2023@gmail.com>
2026-05-29 18:59:56 +0900

    Update project

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callback()
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        $user = User::where('google_id', $googleUser->getId())
+            ->orWhere('email', $googleUser->getEmail())
+            ->first();
+
+        if (! $user) {
+            $user = User::create([
+                'name' => $googleUser->getName() ?? 'Google User',
+                'email' => $googleUser->getEmail(),
+                'google_id' => $googleUser->getId(),
+                'password' => bcrypt(str()->random(32)),
+            ]);
+        } else {
+            if (! $user->google_id) {
+                $user->google_id = $googleUser->getId();
+                $user->save();
+            }
+        }
+
+        Auth::login($user, true);
+
+        return redirect()->route('dashboard');
+    }
+}
