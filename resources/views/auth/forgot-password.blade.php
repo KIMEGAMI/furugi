81ae17f4e8e422c36b32c260c68a687e64b4fbc0
kimegami_shinji <workshinji2023@gmail.com>
2026-05-31 13:32:21 +0900

    Translate forgot password page to Japanese

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('登録済みのメールアドレスを入力してください。パスワード再設定用のリンクをメールで送信します。') }}
     </div>
 
     <!-- Session Status -->
